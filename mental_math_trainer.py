62b3437ac122ddf3cec4da0a48ebc1b5a5370b2c
cm <c.morchutt@gmail.com>
2019-08-18 22:51:28 +0200

    ADD TURM CALCULATER TRAINER
    calculate first and compare afterwards results, time need will also be printed

--- mental_math_trainer.py
+++ mental_math_trainer.py
@@ -129,5 +129,40 @@ class MM_trainer:
     def set_language(self, lang = "de"):
         pass
         # self.speak.Voice = 
-        
-        
+
+#%%
+import re
+def turm_trainer(n_digits = 4):
+    '''
+    Copyright (C) SH
+    '''
+    num = "0"
+    # for safety max while loop
+    n_max = 10
+    n = 0
+    # in case there are zeros in number -> draw again
+    while len(re.findall(pattern = "0", string = num)) > 0 and n <= n_max:
+        num = str(random.randint(10 ** (n_digits - 1), 10 ** (n_digits) - 1))
+        n = n + 1
+    print("Calculate: {}".format(num))
+    start = time.clock()
+    input("Press <ENTER> for solution..")
+    end = time.clock()
+    print("\nTime: {} sec ({} min)".format(np.round(end-start, 1), np.round((end-start)/60, 2)))
+    print("--" * 40)
+    tmp = int(num)
+    print("\n{} * 2".format(tmp))
+    for i in range(2, 10):
+        tmp = tmp * i
+        if i == 9:
+            print("\n{} : 2".format(tmp))
+        else:
+            print("\n{} * {}".format(tmp, i + 1))
+    tmp = tmp / 2
+    for i in range(3, 10):
+        print("\n{} : {}".format(int(tmp), i))
+        tmp = tmp / i
+    print("\n{}".format(int(tmp)))
+    return(True)
+    
+    
