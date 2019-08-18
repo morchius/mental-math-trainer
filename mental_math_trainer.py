59b9889497fc59a07f6a9fa9f9cfbcba2663a931
cm <c.morchutt@gmail.com>
2019-08-18 17:20:48 +0200

    START TRANSFORM MATH TRAINER TO CLASS
    create mental_math_trainer class so we can keep record of stats like average time needed etc.

--- mental_math_trainer.py
+++ mental_math_trainer.py
@@ -70,10 +70,64 @@ def run_training(numb_of_rounds):
             count = count - int(res.split()[1])
         else:
             count = count + int(res.split()[1])
-        key = input("Want to stop? Press 0!\nPress any key to continue!")
+        key = input("Want to stop? Press 0!\nPress <ENTER> to continue!")
         if key == 0:
             control = "stop"
         n += 1
         if (control == "stop") or (n == numb_of_rounds):
             print("Count is: {}".format(count))
     return(count)
+
+    
+#%%
+import win32com.client as wincl
+import numpy as np
+import time
+# make mental math trainer class
+class MM_trainer:
+    def __init__(self):
+        self.speak = wincl.Dispatch("Sapi.SpVoice")
+        self.speak.Rate = .01
+        self.voices = self.speak.GetVoices()
+        self.avg_time_sec = 0
+        self.total_time_sec = 0
+        self.n_questions = 0
+        self.times = "mal"
+        self.n_correct_questions = 0
+        self.answer = None
+        self.solution = None
+        
+    # get two by two question
+    def two_by_two(self):
+        rand = np.random.randint(10,100,size=2)
+        self.solution = str(rand[0] * rand[1])
+        print("\n{} * {}".format(rand[0], rand[1]))
+        self.speak.Speak("{} {} {}".format(rand[0], self.times, rand[1]))
+        start = time.clock()
+        self.answer = input("\nEnter solution followed by <ENTER>")
+        end = time.clock()
+        # check if answer is correct
+        if self.solution == self.answer:
+            print("\nCorrect answer! Well done!")
+            self.speak.Speak("Korrekt, sehr gut, weiter so!")
+            self.n_correct_questions = self.n_correct_questions + 1
+        else:
+            print("\nNot correct :(")
+            self.speak.Speak("Falsch, Brudi")
+        print("\n{}".format(rand[0] * rand[1]))
+        self.speak.Speak("{}".format(rand[0]*rand[1]))
+        # update total time needed
+        self.total_time_sec = self.total_time_sec + end - start
+        # update number of questions
+        self.n_questions = self.n_questions + 1
+        # update average
+        self.avg_time_sec = self.total_time_sec / self.n_questions
+        # print results
+        print("\nTime: {} sec ({} min)".format(np.round(end-start, 1), np.round((end-start)/60, 2)))
+        print("\n--------\nAvg Time: {} sec".format(np.round(self.avg_time_sec, 1)))
+    
+    def set_language(self, lang = "de"):
+        pass
+        # self.speak.Voice = 
+        
+        
